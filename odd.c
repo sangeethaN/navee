26f5db1fa18553984c2939d64bacfad4ce503a5f
sangeethaN <sangenandhu@gmail.com>
2016-06-23 10:05:24 +0530

    Update odd.c

--- odd.c
+++ odd.c
@@ -5,7 +5,7 @@ int main()
     int number;
     scanf("%d",&number);
     if(number%2==0)
-    printf("\nevev number");
+    printf("\neven number");
     if(number%2!=0)
     printf("\nodd number");
    
