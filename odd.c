11ea2aad47b493b8344af466af54560c3500c2bc
sangeethaN <sangenandhu@gmail.com>
2016-06-23 10:04:00 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    scanf("%d",&number);
+    if(number%2==0)
+    printf("\nevev number");
+    if(number%2!=0)
+    printf("\nodd number");
+   
+    return 0;
+}
